94345e95b3e598270c852bbc1eea7f9de4b5b616
王博闻 <151504234+MisakuraPW@users.noreply.github.com>
2026-08-24 13:33:42 +0800

    Export VideoMAE baseline model

--- models/__init__.py
+++ models/__init__.py
@@ -3,13 +3,16 @@
 from .downstream import EchoEFFineTuner, EchoSegFineTuner, load_pretrained_rmae
 from .echo_rmae import EchoRMAE, build_echo_rmae
 from .echo_single_frame_mae import EchoSingleFrameMAE, build_echo_single_frame_mae
+from .video_mae import EchoVideoMAE, build_echo_videomae
 
 __all__ = [
     "EchoEFFineTuner",
     "EchoRMAE",
     "EchoSingleFrameMAE",
+    "EchoVideoMAE",
     "EchoSegFineTuner",
     "build_echo_rmae",
     "build_echo_single_frame_mae",
+    "build_echo_videomae",
     "load_pretrained_rmae",
 ]
